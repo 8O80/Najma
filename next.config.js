622599eefe3a137f103193692ec16485eece6229
iRashed <rashedr1v@gmail.com>
2024-09-09 11:35:39 -0400

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['najma.cloud'], // Replace with your domain
+    },
+  };
+  
+  module.exports = nextConfig;
+  
